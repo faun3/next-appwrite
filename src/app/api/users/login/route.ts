d7b5cb7279a18bc36d99ee0edc5fa1a11b02adfb
faun3 <antonioculcea@gmail.com>
2023-08-17 15:23:05 +0300

    add login api route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,9 @@
+import { connect } from "@/db/db";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { HydratedDocument } from "mongoose";
+
+const GET = async () => {};
+
+export default GET;
