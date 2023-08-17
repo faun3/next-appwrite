4f92f9c833e4a5d9740e24c89ec3f58e9ec0145c
faun3 <antonioculcea@gmail.com>
2023-08-17 15:29:50 +0300

    write part of api user login route

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -3,7 +3,17 @@ import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import { HydratedDocument } from "mongoose";
+import errorifier from "@/helpers/errorifier";
 
-const GET = async () => {};
+const POST = async () => {
+  try {
+    // TODO
+    // write this part
+    // TODO
+  } catch (error) {
+    const errorifiedValue = errorifier(error);
+    throw errorifiedValue;
+  }
+};
 
-export default GET;
+export default POST;
