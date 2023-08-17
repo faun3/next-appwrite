62ce86e728e7749e2ac13544359a47c95d822d00
faun3 <antonioculcea@gmail.com>
2023-08-17 12:10:43 +0300

    login page frontend done ig

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,27 +8,15 @@ const Page = () => {
   const [user, setUser] = useState({
     email: "",
     password: "",
-    username: "",
   });
 
-  const onSignup = async () => {};
+  const onLogin = async () => {};
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-5xl font-bold tracking-wide my-10 text-sky-500">
         Login
       </h1>
-      <label htmlFor="username">Username</label>
-      <input
-        className="px-4 py-2 rounded-lg border-gray-300 focus:border-gray-600 focus:outline-none mb-4 shadow-md"
-        type="text"
-        id="username"
-        value={user.username}
-        onChange={(e) => {
-          setUser({ ...user, username: e.target.value });
-        }}
-        placeholder="johnnydoe6381"
-      />
       <label htmlFor="email">Email</label>
       <input
         className="px-4 py-2 rounded-lg border-gray-300 focus:border-gray-600 focus:outline-none mb-4 shadow-md"
@@ -52,7 +40,7 @@ const Page = () => {
       />
       <button
         className="px-4 py-2 bg-sky-400 hover:bg-sky-600 active:bg-sky-700 active:scale-95 focus:outline-none rounded-lg shadow-md text-orange-50 text-lg font-bold tracking-wide"
-        onClick={onSignup}>
+        onClick={onLogin}>
         Join
       </button>
       <p className="text-gray-500 my-6">
