be302e6a15b7f42bcc4e5c184aa9ab377c5c93db
faun3 <antonioculcea@gmail.com>
2023-08-18 12:48:23 +0300

    add protected routes based on authentication

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+
+  const isPublicPath = path === "/login" || path === "/signup";
+
+  const token = req.cookies.get("token")?.value || "";
+
+  // if the user is logged in (i.e. has a token with a non empty value)
+  // and the path is public
+  // => we redirect them to their profile page
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/profile", req.nextUrl));
+  }
+
+  // if the user is not logged in (the token does not exist or has an empty value)
+  // and the path is private
+  // => we redirect them to the login page
+  if (!(isPublicPath || token)) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+}
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+};
