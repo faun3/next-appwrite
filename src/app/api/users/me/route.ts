c21d52f62d6b927e7e370c674b86120e6b9e6526
faun3 <antonioculcea@gmail.com>
2023-08-18 14:14:01 +0300

    write me api route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,3 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import getDataFromToken from "@/helpers/getDataFromToken";
+import User from "@/models/userModel";
+import { connect } from "@/db/db";
+import errorifier from "@/helpers/errorifier";
 
-export default async function GET() {}
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    await connect();
+
+    const decoded = await getDataFromToken(req);
+
+    const foundUser = await User.findById(decoded.id).select("-password");
+
+    return NextResponse.json({
+      message: "user found",
+      data: foundUser,
+    });
+  } catch (error) {
+    const e = errorifier(error);
+    return NextResponse.json({ error: e.message });
+  }
+}
